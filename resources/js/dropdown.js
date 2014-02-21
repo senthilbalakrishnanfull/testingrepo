b6198a8f512199be46d2da1773d8152773922d80
Vinoth <vinoth@V8S-SS-MAC-PRO.local>
2014-02-21 22:16:27 +0530

    idp and home page changes

--- resources/js/dropdown.js
+++ resources/js/dropdown.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	enableSelectBoxes();
 	$('body, .bdy').click(function(e){
 	if(e.target.className!='selected' && e.target.className!='selectArrow')
-		$('.selectOptions').css('z-index','-1');
+		$('.selectOptions').css('display','none');
  });		 
 });
 
@@ -14,17 +14,23 @@ function enableSelectBoxes(){
 		$(this).attr('value',$(this).children('div.selectOptions').find('ul').find('li:first').html());
 		
 		$(this).children('span.selected, span.selected b, code.selectArrow').click(function(){
-			if($(this).parent().children('div.selectOptions').css('z-index') == '-1'){
-				$(this).parent().children('div.selectOptions').css('z-index','9');
+			if($(this).parent().children('div.selectOptions').css('display') == 'none'){
+				$(this).parent().children('div.selectOptions').css('display','block');
+
+				var customScrollbar=$(this).parent().children('div.selectOptions').find(".mCSB_scrollTools");
+				customScrollbar.css({"opacity":0});
+				$(this).parent().children('div.selectOptions').mCustomScrollbar("update");
+				customScrollbar.animate({opacity:1},"slow");
+
 			}
 			else
 			{
-				$(this).parent().children('div.selectOptions').css('z-index','-1');
+				$(this).parent().children('div.selectOptions').css('display','none');
 			}
 		});
 		
 		$(this).find('ul').find('li.selectOption').click(function(){
-			$(this).parent().parent().css('z-index','-1');
+			$(this).parent().parent().css('display','none');
 			$(this).closest('div.selectBox').attr('value',$(this).attr('value'));
 			$(this).closest('div.selectBox').find('span.selected').html($(this).html());
 			//$(this).parent().parent().siblings('span.selected').html($(this).html());
@@ -32,7 +38,7 @@ function enableSelectBoxes(){
 		
 		$(this).click(function(){
 			//$(this).parent().siblings('div.selectBox').find('div.selectOptions').hide();
-			$(this).parent().siblings().find('div.selectOptions').css('z-index','-1');
+			 $(this).parent().siblings().find('div.selectOptions').css('display','none');
 		});
 		
 	});	
